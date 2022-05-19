c39abbc526ffe451dd775007c337220ef65dcbb1
mikeaalv <yue.wu@uga.edu>
2022-05-19 10:15:42 -0400

    Update dara_pre.r

--- scripts/inference/dara_pre.r
+++ scripts/inference/dara_pre.r
@@ -6,11 +6,14 @@ options(digits=15)
 require(stringr)
 require(magrittr)
 require(fs)
-paredir="/work/aelab/AMF/AMF Imaging/0_Image_Collection/ZEISS Primo Star/Georgia"
-tardir="/scratch/yw44924/amf_inference/data/test"
+paredir="/work/aelab/AMF/AMF Imaging/0_Image_Collection/ZEISS Primo Star/Georgia/2021/Experiment002_Greenhouse_14_Accession/1_JPEG/"
+tardir="/scratch/yw44924/amf_inference2/data/test"
 setwd(tardir)
 matchpat="*.jpg"
+except='Additional images and duplicates'
 listfiles=list.files(path=paredir,pattern=matchpat,full.names=TRUE,recursive=TRUE)
+ind_expt=str_which(string=listfiles,pattern=fixed(except))
+listfiles=listfiles[-ind_expt]
 seq(length(listfiles)) %>% paste0(.,".jpg") -> newfiles
 infortabe=data.frame(location=listfiles,filenames=newfiles)
 file_copy(listfiles,newfiles,overwrite=TRUE)
